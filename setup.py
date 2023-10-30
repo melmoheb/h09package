6a17a0747f267050710c7a0aa95a1111efc65846
Mohamad El Moheb <krb3ym@virginia.edu>
2023-10-29 21:27:52 -0400

    new package

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-	name = 'Booklover',
+	name = 'booklover',
 	version='0.1',
 	description = 'A package for booklovers',
 	url='https://github.com/melmoheb/h09package',
 	author='Mohamad El Moheb',
 	author_email='krb3ym@virginia.edu',
 	license='LICENSE',
-	packages=['booklover', 'booklover_test'],
+	packages=['booklover'],
     install_requires = ['pandas']
 )
